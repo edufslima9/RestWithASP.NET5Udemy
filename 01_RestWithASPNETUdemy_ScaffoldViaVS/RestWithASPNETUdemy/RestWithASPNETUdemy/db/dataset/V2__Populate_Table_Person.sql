fcc7c4b73f1c79dfe23cc859f807f145ce4f9bbc
Eduardo Felipe <edufslima9@gmail.com>
2020-11-24 18:33:37 -0300

    Adding migrations and seeders ('Persons' and 'Books' tables)

---
+++ 01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Allahabad - Índia', 'Indira', 'Female', 'Gandhi'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'Da Vinci');
