a382ea01127880529f552faae8fdf3d4c383f73f
Eduardo Felipe <edufslima9@gmail.com>
2020-11-26 16:54:56 -0300

    Custom serialization

--- 01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ 01_RestWithASPNETUdemy_ScaffoldViaVS/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -3,16 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+        [JsonIgnore]
         public string Address { get; set; }
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
